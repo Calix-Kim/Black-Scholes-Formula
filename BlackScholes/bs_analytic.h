393b667cbadc849a3eb26c97eb31dffb44d907e9
calix <jykcalix@gmail.com>
2021-08-31 19:29:31 +0900

    Simple function for Black-Scholes formula & Vega

---
+++ BlackScholes/bs_analytic.h
@@ -0,0 +1,9 @@
+#pragma once
+
+enum OptionType { Call = 1, Put = -1 };
+
+double bsprice(double s, double k, double r, double  q, double  t, double sigma, OptionType type);	
+
+double bsvega(double s, double k, double r, double  q, double  t, double sigma);
+
+double imivol(double s, double k, double r, double q, double t, double price, OptionType type);
---
+++ BlackScholes/normal.h
@@ -0,0 +1,5 @@
+#pragma once
+
+double normpdf(double x, double mu = 0, double sigma = 1);  // 함수 원형(prototype)
+double normcdf(double x, double mu = 0, double sigma = 1);
+

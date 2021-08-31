adf363ec863350a9ae55c4bc8ca6c71b3746677a
calix <jykcalix@gmail.com>
2021-08-31 19:45:52 +0900

    Update simple function for Black-Scholes formula & Vega

--- BlackScholes/bs_analytic.cpp
+++ BlackScholes/bs_analytic.cpp
@@ -2,11 +2,11 @@
 #include "normal.h"
 #include <cmath>
 
-double bsprice(double s, double k, double r, double  q, double  t, double sigma, OptionType type) {	//call:type = +1 or put:type = - 1
+double bsprice(double s, double k, double r, double  q, double  t, double sigma, OptionType type) {	//call:type = +1 or put:type = -1
 	double d1 = (std::log(s / k) + (r - q + 0.5 * sigma * sigma) * t) / (sigma * std::sqrt(t));
 	double d2 = d1 - sigma * std::sqrt(t);
-	double nd1 = normcdf(type*d1);	//normcdf(-d1)
-	double nd2 = normcdf(type*d2);	//normcdf(-d2)
+	double nd1 = normcdf(type*d1);	
+	double nd2 = normcdf(type*d2);
 
 	double price = type * (s * std::exp(-q * t) * nd1 - k * std::exp(-r * t) * nd2);
 
--- BlackScholes/bs_analytic.h
+++ BlackScholes/bs_analytic.h
@@ -3,7 +3,5 @@
 enum OptionType { Call = 1, Put = -1 };
 
 double bsprice(double s, double k, double r, double  q, double  t, double sigma, OptionType type);	
-
 double bsvega(double s, double k, double r, double  q, double  t, double sigma);
-
 double imivol(double s, double k, double r, double q, double t, double price, OptionType type);
